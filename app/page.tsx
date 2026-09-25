ae986c8d48b16ae518a4d28312bc082507521411
Nathan-s-Anthony <nsanthony95@gmail.com>
2026-09-25 16:29:24 +0200

    rm

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "./components/hero";
 import PageContent from "./components/content";
 import { NavigationEventsProvider } from "./providers/navigationProvider";
 import GSAPCarousel from "./components/gsap/carousel";
+import Footer from "./components/footer";
 
 export default function Home() {
   return (
